e43a3847b850f7a27698b9d56ad61d740f917531
Serkan Hiziroglu <71054911+serkanhiziroglu@users.noreply.github.com>
2023-08-30 07:04:45 +0000

    Submission for Module09 - Serkan Hiziroglu

---
+++ mern/hos09-client/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+ 
+// We use Route in order to define the different routes of our application
+import { Route, Routes } from "react-router-dom";
+ 
+// We import all the components we need in our app
+// App.js
+import Navbar from "./navbar";
+import RecordList from "./recordList";
+import Edit from "./edit";
+import Create from "./create";
+ 
+const App = () => {
+ return (
+   <div>
+     <Navbar />
+     <Routes>
+       <Route exact path="/" element={<RecordList />} />
+       <Route path="/edit/:id" element={<Edit />} />
+       <Route path="/create" element={<Create />} />
+     </Routes>
+   </div>
+ );
+};
+ 
+export default App;
